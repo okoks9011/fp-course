23c0684d43696a65fb261bc3ee4379b4b489374f
eazy <okoks9011@gmail.com>
2019-12-18 14:23:18 +0900

    Solve File IO

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -102,30 +102,31 @@ printFiles = foldRight handle (return ())
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile filePath = (\contents -> return (filePath, contents)) =<< readFile filePath
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles filePaths = sequence $ getFile <$> filePaths
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run filePath = do
+  contents <- readFile filePath
+  filePaths <- getFiles $ lines contents
+  printFiles filePaths
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = do
+  args <- getArgs
+  run $ headOr Nil args
 
 ----
 
