34910f52e44e93c59b6133bb8d1362a2959e4e7e
eazy <okoks9011@gmail.com>
2020-02-11 20:05:00 +0900

    Solve Compose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -16,20 +16,37 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) ::
+    (a -> b)
+    -> Compose f g a
+    -> Compose f g b
+  h <$> Compose fga = Compose $ (h <$>) <$> fga
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure ::
+    a
+    -> Compose f g a
+  pure = Compose . pure . pure
+
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (<*>) ::
+    Compose f g (a -> b)
+    -> Compose f g a
+    -> Compose f g b
+  Compose fgab <*> Compose fga = Compose $ (<*>) <$> fgab <*> fga
+
+runCompose ::
+  Compose f g a
+  -> f (g a)
+runCompose (Compose c) = c
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
 -- Implement the (=<<) function for a Monad instance for Compose
-  (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+  (=<<) ::
+    (a -> Compose f g b)
+    -> Compose f g a
+    -> Compose f g b
+  (=<<) = error "Course.Compose (<<=)#instance (Compose f g), Impossible"
